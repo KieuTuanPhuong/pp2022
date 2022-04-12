7c065ffb3446fa5c8533d301260908c96a0eec55
KieuTuanPhuong <maingmame23114@gmail.com>
2022-04-13 05:24:30 +0700

    Fixing codes

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,66 +1,80 @@
-def get_num_Student():
-    num_stu = int(input("Enter how many student: "))
-    return num_stu
+student_data = {}
+course_data = {}
 
-def stu_info(num_stu):
-    students = []
-    for i in range(num_stu):
-        print(f"Enter student #{i+1}:")
-        id = input("- ID: ")
-        name = input("- Name: ")
-        dob = input("- DoB: ")
-        students.append({"Id": id, "Name": name, "DoB": dob})
-    return students
 
-def num_course():
-    return int(input("Enter the number of courses: "))  
+def display_menu():
+    print("======================================")
+    print(" Welcome to Student Management System")
+    print("======================================")
+    print("1. Add New Student")
+    print("2. View Students")
+    print("3. Add New Course")
+    print("4. View Courses")
+    print("5. Add Student Marks")
+    print("6. Mark List")
+    print("7. Quit")
 
-def course_info(num_course):
-    courses = []
-    for i in range(num_course):
-        course_id = int(input(" * Course ID: "))
-        course_name = input(" * Name of course: ")
-        courses.append({"id": course_id, "name": course_name})
-    return courses
+def add_student(in4):
+    num_std = int(input("How many Students: "))
+    for i in range(1, num_std+1):
+        print("Enter data for student #",i,": ")
+        id = input("Enter student's ID: ")
+        if not found(in4, id):
+            name = input("Enter student's Name: ")
+            dob = input("Enter student's Date of Birth: ")
+            in4[id] = [name,dob]
+        else:
+            print(f"ID Existed!!!")
 
-def choose_course():
-    return input("Choose a course(ID): ")
+def student_list():
+    print("{:15} | {:25} | {:25}".format("ID", "Name", "Date of Birth"))
+    for key in student_data:
+        print("{:15} | {:25} | {:25}".format(key, student_data[key][0], student_data[key][1]))
 
-def course_mark(id, students):
-    course_mark = []    
-    i = 1
-    for student in students:
-        print(f"Enter student #{i} mark: ")
-        print(f"-ID: {student['id']}, Name: {student['name']}, DoB: {student['dob']}")
-        mark = int(input(" * Mark: "))
-        course_mark.append({"c_id": id,"s_id": student['id'], "name": student['name'], "DoB": student['dob'], "Mark": mark})
-        i += 1
-    return course_mark
 
-def course_list(courses):
-    print("List of all Courses:")
-    for course in courses:
-        print(f"-ID: {course['course_id']}, Name: {course['course_name']}")
+def add_course(in4):
+    num_crs = int(input("How many Courses: "))
+    for i in range(1, num_crs+1):
+        print("Enter data for course #",i,": ")
+        id = input("Enter course's ID: ")
+        if not found(in4, id):
+            name = input("Enter course's Name: ")
+            in4[id] = [name]
+        else:
+            print(f"ID Existed!!!")
 
-def student_list(students):
-    print("Student List:")
-    for student in students:
-        print(f"-ID: {student['id']}, Name: {student['name']}, DoB: {student['dob']}")
+def course_list():
+    print("{:15} | {:25}".format("ID", "Name")
+    for key in course_data:
+        print("{:15} | {:25}".format(key, course_data[key][0]))
 
-def display_marks(id, course_mark):
-    print(f"Listing course ID {id} marks:")
-    for mark in course_mark:
-        if(id == mark['c_id']):
-            print(f"- Student ID: {mark['s_id']}, Name: {mark['name']}, DoB: {mark['DoB']}, Mark: {mark['Mark']}")
 
-student_num = get_num_Student()
-students = stu_info(student_num)
-student_list(students)
+def empty(dictionary):
+    if len(dictionary) == 0:
+        return True
+    return False
 
-course_num = num_course()
-courses = course_info(course_num)
-course_list(courses)
+def found(dictionary, id):
+    if id in dictionary:
+        return True
+    return False
 
-Choosen_course_ID = choose_course()
-course_marks = course_mark(Choosen_course_ID, students)
-display_marks(Choosen_course_ID,course_marks)
+
+
+
+while True:
+    display_menu()
+
+    choice = input("Enter your choice: ")
+    if choice == '1':
+        add_student(in4=student_data)
+    elif choice == '2':
+        add_course(in4=course_data)
+    elif choice == '3':
+        search_student()
+    elif choice == '4':
+        update_student()
+    elif choice == '5':
+        delete_student()
+    else:
+        break
